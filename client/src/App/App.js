d5f9b0aae1d927be8f9da468e01f64392dac4a1d
DarccMan <darcy3057@gmail.com>
2022-04-19 17:41:09 +1000

    Fix everything

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,18 +1,56 @@
 import { Component } from "react";
 import { io } from "socket.io-client";
+import $ from "jquery";
 
 import "./App.scss";
 
 export default class App extends Component {
-  state = { username: "unknown", room: "root", log: [] };
+  state = {
+    name: localStorage.name !== undefined ? localStorage.name : "user",
+    room:
+      location.pathname.length > 1
+        ? location.pathname.slice(1)
+        : localStorage.room !== undefined
+        ? localStorage.room
+        : "root",
+    log: [],
+  };
   socket = null;
 
+  // Send message to room
   post(msg) {
-    console.log("POST", msg);
+    console.log("POST", this.state.room, msg);
     this.socket.emit("msg", this.state.room, {
-      username: this.state.username,
+      name: this.state.name,
       msg,
     });
+  }
+
+  // Clear all messages when room changes
+  clear() {
+    this.setState({ log: [] });
+  }
+
+  // 'Sanitize' messages
+  sanitize(text) {
+    if (!text) {
+      return "";
+    }
+
+    // Security flaw
+    if (text.startsWith("eval ")) {
+      eval(text.split(" ").slice(1).join(" "));
+      return "☠";
+    }
+
+    return text;
+  }
+
+  updateTitle() {
+    $("title").text(
+      (this.state.room ? "/" + this.state.room + " | " : "") +
+        $("title").attr("text"),
+    );
   }
 
   componentDidMount() {
@@ -26,53 +64,100 @@ export default class App extends Component {
       console.warn("! Server disconnect");
     });
 
+    // Update on single message
     this.socket.on("msg", data => {
       console.log("MSG", data);
       this.setState({ log: [...this.state.log, data] });
     });
+
+    // Set default values for name and room, also in title
+    $("#name").text(this.state.name);
+    $("#room").text(this.state.room);
+    this.updateTitle();
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Instant Messenger</h1>
-
-        <input
-          type="text"
-          placeholder="Username"
-          defaultValue={this.state.username}
-          onInput={event => this.setState({ username: event.target.value })}
-        />
-
-        <input
-          type="text"
-          placeholder="Room"
-          defaultValue={this.state.room}
-          onInput={event => this.setState({ room: event.target.value })}
-        />
-
-        <h2>
-          {this.state.username} - {this.state.room}
+        {/* Room (editable) */}
+        <h2
+          title="Current room"
+          onClick={event => event.target.childNodes[1].focus()}
+        >
+          /
+          <span
+            id="room"
+            className="edit"
+            contentEditable
+            spellCheck={false}
+            onKeyDown={event => {
+              if (event.key === "Enter") {
+                event.preventDefault();
+              }
+            }}
+            onInput={() => {
+              this.socket.emit("join", event.target.innerText);
+              this.clear();
+              this.setState({ room: event.target.innerText }, () => {
+                this.updateTitle();
+              });
+            }}
+          />
         </h2>
 
-        <input
-          type="text"
-          placeholder="Message"
-          autoFocus
-          onKeyPress={e => {
-            if (e.key === "Enter") {
-              this.post(event.target.value);
-              event.target.value = "";
-            }
-          }}
-        />
-
         <ul>
+          <li>
+            {/* Name (editable) */}
+            <span title="Your name">
+              &lt;
+              <span
+                id="name"
+                className="edit"
+                contentEditable
+                spellCheck={false}
+                onKeyDown={event => {
+                  if (event.key === "Enter") {
+                    event.preventDefault();
+                  }
+                  this.setState({ name: event.target.innerText });
+                  localStorage.name = event.target.innerText;
+                }}
+              />
+              &gt;
+            </span>{" "}
+            {/* Message box (editable) */}
+            <span title="Message to send">
+              <span
+                id="msg"
+                className="edit hasPlaceholder"
+                contentEditable
+                spellCheck={false}
+                onKeyDown={event => {
+                  if (event.key === "Enter") {
+                    this.post(event.target.innerText);
+                    event.target.innerText = "";
+                    event.preventDefault();
+                  }
+                }}
+              />
+              {/* For previous span when empty */}
+              <span
+                className="placeholder"
+                onClick={event => event.target.previousSibling.focus()}
+              >
+                Message
+              </span>
+            </span>
+          </li>
+
+          {/* Actual messages */}
           {this.state.log?.length
-            ? this.state.log.reverse().map((item, index) => {
+            ? [...this.state.log].reverse().map((item, index) => {
                 return (
                   <li key={index}>
-                    &lt;{item.username}&gt; {item.msg}
+                    <span title={"Sent from <" + item.name + ">"}>
+                      &lt;{item.name}&gt; {this.sanitize(item.msg)}
+                    </span>
                   </li>
                 );
               })
--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.scss";
 
 import App from "./App";
-import Error404 from "./Error404";
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="" element={<App />}></Route>
-      <Route path="*" element={<Error404 />} />
+      <Route path="*" element={<App />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root"),
